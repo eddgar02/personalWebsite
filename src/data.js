6553997e699285af6ff441992a66ef8feda575a9
edgarlarasanchez <edgar.lara7681@gmail.com>
2023-09-24 22:53:08 -0500

    update gifs

--- src/data.js
+++ src/data.js
@@ -4,24 +4,24 @@ export const projects = [
     subtitle: "Java",
     description:
       "Created collaborative object-oriented classes for simulating a game involving ants and beetles, complete with a user interface to control rounds, and employed control structures and method overloading for dynamic movement and interactions.",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    image: "./antvbeetle.gif",
+    link: "https://github.com/eddgar02/Projects/tree/main/AntsVsBeetles.com",
   },
   {
     title: "Intergral Calculator",
     subtitle: "Java",
     description:
       "Developed a program using object-oriented programming to read and integrate integrals from a file by implementing binary trees and generics for term separation",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    image: "./intcalc.gif",
+    link: "https://github.com/eddgar02/Projects/tree/main/IntegralCalculator",
   },
   {
     title: "Baseball Record Management",
     subtitle: "Java",
     description:
       "Designed a multi-class object-oriented system for parsing and organizing baseball statistics using linked lists, optimizing algorithms for efficient data processing.",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    image: "./baserec.gif",
+    link: "https://github.com/eddgar02/Projects/tree/main/BaseballRecordManagement",
   },
   
 ];
@@ -32,5 +32,5 @@ export const skills = [
   "Java",
   "C/C++",
   "Javascipt",
-  "MIPS",
+  "CSS",
 ];
