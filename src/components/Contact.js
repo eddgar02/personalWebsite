4339df9db4982a312accdd1ade366f5f33bfe79f
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-23 21:54:42 -0500

    Real centering

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,16 +25,13 @@ export default function Contact() {
   }
 
   return (
-    <section
-      id="contact"
-      className="relative min-h-screen flex items-center justify-center"
-    >
-      <div className="container mx-auto flex justify-center items-center min-h-screen px-5 py-10">
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
           netlify
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          className="mx-auto flex flex-col w-1/3 md:py-8 mt-8"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
