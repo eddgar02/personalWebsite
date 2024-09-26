6debf9952132b64b8859b8159bd60fe5de0ac6a6
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-25 20:03:26 -0500

    Got rid of white space at top of webiste.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="min-h-screen text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
