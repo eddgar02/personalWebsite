ca0bf96ef01dc027e30697e447b4b1d9b0fde90c
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-10-11 18:05:00 -0500

    Gif for aiBlog project now working properly.

--- src/data.js
+++ src/data.js
@@ -20,8 +20,7 @@ export const projects = [
     subtitle: "Python",
     description:
       "Engineered a web application to generate blog posts from any YouTube link.",
-    image:
-      "https://private-user-images.githubusercontent.com/124748948/367429352-6f2f5027-8ebb-44e8-8100-d98b8ac9930e.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mjc0NzUzMTAsIm5iZiI6MTcyNzQ3NTAxMCwicGF0aCI6Ii8xMjQ3NDg5NDgvMzY3NDI5MzUyLTZmMmY1MDI3LThlYmItNDRlOC04MTAwLWQ5OGI4YWM5OTMwZS5naWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwOTI3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDkyN1QyMjEwMTBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yYjU1MTA5ODZjMzJkMTk4OWVkNDEyMmM0MDI0YzY2ZjllNTMxYTA3NDUwMWU5YmIzZWUxMGYyM2UzNmM1ZTA1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.8_BV1ucsHCtvcBvw3c8sBDGSRWO9JTucUK94pz1czPg",
+    image: "./aiblog.gif",
     link: "https://github.com/eddgar02/AIYoutubeBlogApp",
   },
 ];
